619b0c53bb817fc138cf5bd26eb1a9a001d5befb
Bintehassan <mariam.bintehassan@gmail.com>
2019-12-26 13:19:53 +0100

    Sticky navigation complete w jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,34 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  // For sticky navigation
+  $('.js--section-features').waypoint(
+    function(direction) {
+      if (direction == 'down') {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    },
+    {
+      offset: '60px;'
+    }
+  );
 
+  // Scroll on buttons
+  $('.js--scroll-to-plan').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.js--section-plans').offset().top
+      },
+      1000
+    );
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.js--section-features').offset().top
+      },
+      1000
+    );
+  });
 });
