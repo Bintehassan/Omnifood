adf68d63fb664e0acb7e803094a096ba3ef3830c
Bintehassan <mariam.bintehassan@gmail.com>
2019-12-27 00:02:53 +0100

    added animations on scroll w css and jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -57,4 +57,41 @@ $(document).ready(function() {
       );
     } // End if
   });
+
+  //Animations on scrolling and other effects
+  $('.js--wp-1').waypoint(
+    function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-2').waypoint(
+    function(direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-3').waypoint(
+    function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%'
+    }
+  );
+
+  $('.js--wp-4').waypoint(
+    function(direction) {
+      $('.js--wp-4').addClass('animated pulse');
+    },
+    {
+      offset: '50%'
+    }
+  );
 });
